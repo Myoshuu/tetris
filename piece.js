0b5f02e0e181eda73ade5328a4be2002d43635ad
Joevano <myoshu.me@gmail.com>
2022-05-20 14:00:40 +0700

    add: tetris

---
+++ piece.js
@@ -0,0 +1,19 @@
+class Piece {
+  constructor(shape) {
+    this.shape = shape
+    this.ctx = this.ctx
+    this.x = 0
+    this.y = Math.random(COLS / 2)
+  }
+
+  renderPiece() {
+    this.shape.map((row, i) => {
+      row.map((cell, j) => {
+        if (cell > 0) {
+          this.ctx.fillStyle = COLORS[cell]
+          this.ctx.fillRect(this.x + j, this.y + i, 1, 1)
+        }
+      })
+    })
+  }
+}
